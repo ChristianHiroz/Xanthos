6cbbfed677b4bcc151d9c2e91e4ed43b6b98e3c3
ChristianHiroz <twinkie.hiroz@gmail.com>
2017-05-26 19:26:52 +0200

    push

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -20,20 +20,24 @@ class DefaultController extends Controller
     {
         /** @var ProductRepository $repository */
         $repository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $repository->getLastProducts();
 
         /** @var User $user */
         $user = $this->getUser();
+        if($user){
+            $amount = 0;
+            $productCount = 0;
 
-        $products = $repository->getLastProducts();
-        $amount = 0;
-        $productCount = 0;
-
-        if($user->getCart()){
-            $cart = $user->getCart();
-            foreach ($cart->getProducts() as $product){
-                $amount += $product->getPrice();
-                $productCount++;
+            if($user->getCart()){
+                $cart = $user->getCart();
+                foreach ($cart->getProducts() as $product){
+                    $amount += $product->getPrice();
+                    $productCount++;
+                }
             }
+        }else {
+            return array('products' => $products, 'categorys' => $this->getDoctrine()->getManager()->getRepository(Category::class)->findBy(array('mainCategory' => true)));
+
         }
         return array('amount' => $amount, 'productCount' => $productCount, 'products' => $products, 'categorys' => $this->getDoctrine()->getManager()->getRepository(Category::class)->findBy(array('mainCategory' => true)));
     }
